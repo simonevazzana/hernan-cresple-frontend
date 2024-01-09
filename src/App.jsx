0fae8e58bffa8479366047b0e7af6d3e5d0c32da
simonevazzana <simo.vazzana@gmail.com>
2024-01-09 10:38:57 +0100

    feat: added main frontend component, with link to Chain game

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Chain from './Chain'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>Welcome!</h1>
+      </header>
+      <Routes>
+        <Route
+          path='/' element={
+            <div>
+              <Link to='/chainpions'>
+                <h3>
+                  Chainpions League
+                </h3>
+              </Link>
+              <Link to='/boardesliga'>
+                <h3>
+                  Boardesliga
+                </h3>
+              </Link>
+            </div>
+        }
+        />
+        <Route path='/chainpions' element={<Chain />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(React.createElement(App))
